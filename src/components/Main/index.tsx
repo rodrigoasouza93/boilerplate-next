d461f329344fca68cc4b1eafc479ea624e71452b
Rodrigo Souza <rodrigo.augusto93@gmail.com>
2021-12-03 19:37:05 -0300

    chore: styled components

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,18 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo - React Avançado escrito ao lado."
+    />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>Typescript, ReactJS e Styled Components</S.Description>
+    <S.Ilustration
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvedor de frente para a tela com código."
+    />
+  </S.Wrapper>
 )
 
 export default Main
